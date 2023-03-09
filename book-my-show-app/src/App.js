9570e588d9904a656c7b250d54a09fb3e847527e
abarna10303 <abarnas361@gmail.com>
2023-03-09 09:37:50 +0530

    Book my show

---
+++ book-my-show-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './Header/Header';
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
